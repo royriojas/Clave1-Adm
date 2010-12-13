2d7a7c4db0cf78a1504d2b593812fdbbb5aa49d0
Roy Riojas <royriojas@gmail.com>
2010-12-12 16:12:09 -0800

    Initial Commit

---
+++ BLL/BLL/Contracts/IUserManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BLL.Contracts
+{
+  interface IUserManager
+  {
+
+  }
+
+  internal interface IAuditoriaCasos
+  {
+    CasoMedico Find();
+  }
+
+  public class CasoMedico
+  {
+    public String Codigo { get; set; }
+    public DateTime? Fecha { get; set; }
+    public String Auditor { get; set; }
+    public String Estado { get; set; }
+    public String TotalGasto { get; set; }
+    public String Titular { get; set; }
+  }
+}
